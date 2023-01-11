168f998592fec77f2f5e888400f86c50fcb15a6f
鄭清源 <crazy653555@gmail.com>
2023-01-11 14:11:51 +0800

    [feat] Model資料自動綁appsettings.json中的內容

--- OAuth/OAuth.Line.Core/CoreServiceCollectionExtenstions.cs
+++ OAuth/OAuth.Line.Core/CoreServiceCollectionExtenstions.cs
@@ -6,11 +6,20 @@ namespace OAuth.Line.Core;
 
 public static class CoreServiceCollectionExtenstions
 {
+    /// <summary>
+    /// 將 Line Login 和 Line Notify 及相關聯動的核心程式加入 DI Container 中
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+
     public static void AddCoreLibs(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddHttpClient();
 
+        //設定Mapper資料，Mapper到OAth.Web中的appsettings.json
+        services.Configure<LineLoginConfig>(configuration.GetSection("LineLogin"));
+
+        //設定class
         services.AddScoped<LineLoginService>();
-        services.AddScoped<LineLoginConfig>();
     }
 }
--- OAuth/OAuth.Web/Controllers/HomeController.cs
+++ OAuth/OAuth.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OAuth.Web.Models;
 using System.Diagnostics;
+using System.Text.Encodings.Web;
+using Microsoft.Extensions.Options;
 using OAuth.Line.Core.LineLogin;
 
 namespace OAuth.Web.Controllers
@@ -8,23 +10,32 @@ namespace OAuth.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly LineLoginConfig _lineLoginConfigOptions;
         private readonly LineLoginService _lineLoginService;
-        private readonly LineLoginConfig _lineLoginConfig;
 
-        //private string _lineLoginRedirectUri
-        //{
-        //    get { return $"{Request.Scheme}://{Request.Host}{Request.PathBase}{_lineLoginConfig.ReturnPath}"; }
-        //}
+        private string _lineLoginRedirectUri
+        {
+            get
+            {
+                var requestScheme = Request.Scheme;
+                var requestHost = Request.Host;
+                var requestPathBase = Request.PathBase;
+                var returnPath = _lineLoginConfigOptions.ReturnPath;
+
+                return $"{requestScheme}://{requestHost}{requestPathBase}{returnPath}";
+            }
+        }
 
         public HomeController(
             ILogger<HomeController> logger,
-            LineLoginService lineLoginService,
-            LineLoginConfig lineLoginConfig
-            )
+            IOptions<LineLoginConfig> lineLoginConfigOptions,
+            LineLoginService lineLoginService
+
+        )
         {
             _logger = logger;
+            _lineLoginConfigOptions = lineLoginConfigOptions.Value;
             _lineLoginService = lineLoginService;
-            _lineLoginConfig = lineLoginConfig;
         }
 
         public IActionResult Index()
@@ -37,7 +48,7 @@ namespace OAuth.Web.Controllers
             var state = "3393";
 
             // 轉到 Line Login 登入網址
-            //var lineLoginUrl = _lineLoginService.GenerateLineLoginUrl(_lineLoginConfig.ChannelId, UrlEncoder.Default.Encode(_lineLoginRedirectUri), state);
+            var lineLoginUrl = _lineLoginService.GenerateLineLoginUrl(_lineLoginConfigOptions.ChannelId, UrlEncoder.Default.Encode(_lineLoginRedirectUri), state);
 
             //return Redirect(lineLoginUrl);
 
