475610d8c0280b088eecfed44eb776fda686748c
鄭清源 <crazy653555@gmail.com>
2023-01-11 18:29:01 +0800

    [feat] 解析IDToken(OIDC)

---
+++ OAuth/OAuth.Line.Core/LineLogin/IdToken.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OAuth.Line.Core.LineLogin;
+
+public class IdToken
+{
+    [JsonPropertyName("iss")]
+    public string Iss { get; set; }
+
+    [JsonPropertyName("sub")]
+    public string Sub { get; set; }
+
+    [JsonPropertyName("aud")]
+    public string Aud { get; set; }
+
+    [JsonPropertyName("exp")]
+    public int Exp { get; set; }
+
+    [JsonPropertyName("iat")]
+    public int Iat { get; set; }
+
+    [JsonPropertyName("amr")]
+    public List<string> Amr { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("picture")]
+    public string Picture { get; set; }
+}
--- OAuth/OAuth.Web/Controllers/HomeController.cs
+++ OAuth/OAuth.Web/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OAuth.Web.Models;
 using System.Diagnostics;
+using System.Text;
 using System.Text.Encodings.Web;
+using System.Text.Json;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Options;
 using OAuth.Line.Core.LineLogin;
 
@@ -73,7 +76,48 @@ namespace OAuth.Web.Controllers
             // 透過 code 取得 access token
             var accessToken = await _lineLoginService.GetAccessTokenAsync(code, _lineLoginConfig.ChannelId, _lineLoginConfig.ChannelSecret, _lineLoginRedirectUri);
 
-            throw new Exception();
+            // 取得 id token 物件後，將相關資訊塞到 cookie 中
+            if (TryParseIdToken(accessToken.IdToken, out var idToken))
+            {
+                //呼叫 _lineNotifyBindingService.UpdateLoginAsync 
+
+
+                HttpContext.Response.Cookies.Append("AccessToken", accessToken.AccessToken);
+                HttpContext.Response.Cookies.Append("ExpiresIn", accessToken.ExpiresIn.ToString());
+                HttpContext.Response.Cookies.Append("IdToken", accessToken.IdToken);
+                HttpContext.Response.Cookies.Append("RefreshToken", accessToken.RefreshToken);
+                HttpContext.Response.Cookies.Append("Scope", accessToken.Scope);
+                HttpContext.Response.Cookies.Append("TokenType", accessToken.TokenType);
+
+                return RedirectToAction("Index");
+            }
+
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// 取得 JwtToken 的 payload 部分，型別為 IdToken
+        /// </summary>
+        /// <param name="jwtToken"></param>
+        /// <param name="idToken"></param>
+        /// <returns></returns>
+        public bool TryParseIdToken(string jwtToken, out IdToken idToken)
+        {
+            try
+            {
+                var payloadString = jwtToken.Split(".")[1];
+                var result = JsonSerializer.Deserialize<IdToken>(Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(payloadString)));
+                if (result is not null)
+                {
+                    idToken = result;
+                    return true;
+                }
+            }
+            catch { }
+
+            idToken = null;
+
+            return false;
         }
 
         public IActionResult Privacy()
