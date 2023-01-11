aee2a30e4d5dfb715ab9fe9cc228e59925fbe6e4
鄭清源 <crazy653555@gmail.com>
2023-01-11 12:35:55 +0800

    [feat] 加入專案，DI問題尚未解決

---
+++ OAuth/OAuth.Line.Core/LineLogin/LineLoginConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace OAuth.Line.Core.LineLogin;
+
+public class LineLoginConfig
+{
+    public string ChannelId { get; set; }
+    public string ChannelSecret { get; set; }
+    public string ReturnPath { get; set; }
+
+}
---
+++ OAuth/OAuth.Web/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OAuth.Web.Models;
+using System.Diagnostics;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Server.IIS.Core;
+using OAuth.Line.Core.LineLogin;
+
+namespace OAuth.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly LineLoginService _lineLoginService;
+        //private readonly LineLoginConfig _lineLoginConfig;
+
+        //private string _lineLoginRedirectUri
+        //{
+        //    get { return $"{Request.Scheme}://{Request.Host}{Request.PathBase}{_lineLoginConfig.ReturnPath}"; }
+        //}
+
+        public HomeController(
+            ILogger<HomeController> logger,
+            LineLoginService lineLoginService
+            //LineLoginConfig lineLoginConfig
+            )
+        {
+            _logger = logger;
+            _lineLoginService = lineLoginService;
+            //_lineLoginConfig = lineLoginConfig;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LineLogin()
+        {
+            var state = "3393";
+
+            // 轉到 Line Login 登入網址
+            //var lineLoginUrl = _lineLoginService.GenerateLineLoginUrl(_lineLoginConfig.ChannelId, UrlEncoder.Default.Encode(_lineLoginRedirectUri), state);
+
+            //return Redirect(lineLoginUrl);
+
+            throw new Exception();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
